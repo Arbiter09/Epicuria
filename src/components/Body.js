79361c00badc55fc0e5725f7f5f66ff540ddbbce
Jas Shah <jasshah40@gmail.com>
2025-06-21 03:48:41 -0400

    Added filter functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,44 +1,76 @@
-import React, { useEffect, useState } from 'react'
-import RestaurantCard from './RestaurantCard'
-import {restaurantList} from "../utils/mockData"
-import { SWIGGY_API } from '../utils/constants'
-import Shimmer from './Shimmer'
+import React, { useEffect, useState } from "react";
+import RestaurantCard from "./RestaurantCard";
+import { restaurantList } from "../utils/mockData";
+import { SWIGGY_API } from "../utils/constants";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-
-  const [listOfRestaurants, setListOfRestaurants] = useState([])
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [fiteredListOfRestaurants, setFilteredListOfRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
   const handleTopRestaurants = () => {
-    const newList = listOfRestaurants.filter((res) => res.info.avgRating > 4.5)
-    setListOfRestaurants(newList)
-  }
+    const newList = listOfRestaurants.filter((res) => res.info.avgRating > 4.5);
+    setFilteredListOfRestaurants(newList);
+  };
 
   useEffect(() => {
-    fetchData()
-  },[])
+    fetchData();
+  }, []);
 
   const fetchData = async () => {
-    const data = await fetch(SWIGGY_API)
-    const json = await data.json()
-    console.log(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
-    setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
-    //setListOfRestaurants(json.data.cards[1].card.gridElements.infoWithStyle.restaurants)
-  }
+    const data = await fetch(SWIGGY_API);
+    const json = await data.json();
+    console.log(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setListOfRestaurants(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredListOfRestaurants(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
 
-  if (listOfRestaurants.length === 0){
-    return <Shimmer />
+  if (listOfRestaurants.length === 0) {
+    return <Shimmer />;
   }
 
   return (
-    <div className='body-container'>
-        <div>
-            <button className='top-rated-btn' onClick={() => handleTopRestaurants()}>Top Rated Restaurants</button>
+    <div className="body-container">
+      <div className="filter">
+        <div className="search">
+          <input
+            className="search-box"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <button
+            className="top-rated-btn"
+            onClick={() => {
+              const filteredList = listOfRestaurants.filter((res) =>
+                res?.info?.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilteredListOfRestaurants(filteredList);
+            }}
+          >
+            Search
+          </button>
         </div>
-        <div className='card-container'>
-        {listOfRestaurants.map((restaurant) => <RestaurantCard key={restaurant.info.id} resData={restaurant}/> )}
-        </div>
+        <button
+          className="top-rated-btn"
+          onClick={() => handleTopRestaurants()}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="card-container">
+        {fiteredListOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
