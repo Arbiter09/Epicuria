931af533daab49c9ff10489e9aa40bec4be0e16d
Jas Shah <jasshah40@gmail.com>
2025-07-01 15:46:33 -0400

    Added test cases for ContactUs Page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,22 @@
 const Contact = () => {
   return (
     <div>
-      <h1>Contact</h1>
+      <h1 className="text-center text-2xl m-2">Contact Us</h1>
+      <form>
+        <input
+          className="p-4 m-4 rounded-2xl border border-gray-300"
+          placeholder="name"
+          type="text"
+        />
+        <input
+          className="p-4 m-4 rounded-2xl border border-gray-300"
+          placeholder="message"
+          type="text"
+        />
+        <button className="m-4 p-4 rounded-2xl border border-gray-700 bg-gray-200 cursor-pointer">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
---
+++ src/components/__tests__/contact.test.js
@@ -0,0 +1,33 @@
+import { render, screen } from "@testing-library/react";
+import Contact from "../Contact";
+import "@testing-library/jest-dom";
+
+test("Test to see if Contact Component is being rendered", () => {
+  render(<Contact />);
+
+  const heading = screen.getByRole("heading");
+
+  expect(heading).toBeInTheDocument();
+});
+
+test("Test to see if 2 input boxes are being rendered", () => {
+  render(<Contact />);
+
+  const inputBoxes = screen.getAllByRole("textbox");
+
+  expect(inputBoxes.length).toBe(2);
+});
+
+test("Test should load button onto the component", () => {
+  render(<Contact />);
+
+  const button = screen.getByText("Submit");
+  expect(button).toBeInTheDocument();
+});
+
+test("Test to fetch input box by placeholder (name)", () => {
+  render(<Contact />);
+
+  const name = screen.getByPlaceholderText("name");
+  expect(name).toBeInTheDocument();
+});
